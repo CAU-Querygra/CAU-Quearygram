7c8e2a2b984b587618a38de5eac4a68061b36bee
Leekangeun1 <92072223+Leekangeun1@users.noreply.github.com>
2022-12-04 11:25:02 +0900

    url image load 오류 수정

--- lib/main.dart
+++ lib/main.dart
@@ -43,3 +43,12 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class NoCheckCertificateHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
