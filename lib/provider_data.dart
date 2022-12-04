b7ea1f6f937a1c8f025f97e50206260fc4fae8fa
Leekangeun1 <92072223+Leekangeun1@users.noreply.github.com>
2022-12-04 14:07:31 +0900

    question_id 디폴트값 부여

--- lib/provider_data.dart
+++ lib/provider_data.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class UserData with ChangeNotifier{
+  User? user;
   String id ="BuVJIFMHMmaKDBzA08CIVSMb3Wy2";
   String user_name = "홍길동";
   String email = "cau@cau.ac.kr";
 
-  set_user_data(id, user_name, email){
+  set_user_data(user, id, user_name, email){
+    this.user = user;
     this.id = id;
     this.user_name = user_name;
     this.email=email;
@@ -16,7 +19,7 @@ class UserData with ChangeNotifier{
 
 class LectureData with ChangeNotifier{
   String lecture_id = '';
-  String question_id = '';
+  String question_id = 'rrfAI0l62W2AqAcBUPn6';
 
   set_lecture_id(lecture_id){
     this.lecture_id=lecture_id;
